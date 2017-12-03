37121ed1b3d82a98208820ceb9163e2b49883a65
Dominik Peter <dominik.peter87@gmail.com>
2017-12-03 16:50:00 +0100

    init

--- Week3/ex2/costFunction.m
+++ Week3/ex2/costFunction.m
@@ -22,10 +22,10 @@ grad = zeros(size(theta));
 
 h = sigmoid(X * theta);
 
-J = (1 / m) * sum(-y .* log(h) - (1 - y) .* log(1 - h));
+J = (1 ./ m) * sum(-y .* log(h) - (1 - y) .* log(1 - h));
 % grad = (1 / m) * sum((h - y) .* X);
  
-grad = (1 / m) * (X' * (h - y)); 
+grad = (1 ./ m) * (X' * (h - y)); 
 
 
 % =============================================================
